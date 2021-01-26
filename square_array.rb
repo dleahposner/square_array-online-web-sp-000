14a219ee19665caa682825eda068a8a383b5faef
Devorah Posner <dleahposner@gmail.com>
2021-01-26 17:34:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-      new_array = []
-      array.each do |num|
-        squared_num = num ** 2
-        new_array << (squared_num)
-        # new_array.push(squared_num)
-      end
+   new_array = []
+   array.each do |num|
+   squared_num = num ** 2
+   new_array << (squared_num)
+   # new_array.push(squared_num)
+   end
       return new_array
-    end
+  end
